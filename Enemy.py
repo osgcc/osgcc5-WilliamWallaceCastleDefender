a7f05ce731df472343a335b430ebdbcd1494894c
Kyle Magocs <kylemagocs@gmail.com>
2011-11-06 05:44:26 -0500

    ENEMY SPRITES

--- Enemy.py
+++ Enemy.py
@@ -9,14 +9,16 @@ class Enemy(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.HP = 5
         self.image = 0
-        self.images = [pygame.image.load(os.path.join(os.curdir, 'spritel1.png')).convert_alpha(),
-                      pygame.image.load(os.path.join(os.curdir, 'spritel2.png')).convert_alpha(),
-                      pygame.image.load(os.path.join(os.curdir, 'spritel3.png')).convert_alpha(),
-                      pygame.image.load(os.path.join(os.curdir, 'spritel4.png')).convert_alpha(),
-                      pygame.image.load(os.path.join(os.curdir, 'spritel5.png')).convert_alpha(),
-                      pygame.image.load(os.path.join(os.curdir, 'spritel6.png')).convert_alpha(),
-                      pygame.image.load(os.path.join(os.curdir, 'spritel7.png')).convert_alpha(),
-                      pygame.image.load(os.path.join(os.curdir, 'spritel8.png')).convert_alpha()]
+        self.framenumber = 0
+        self.right = right
+        self.images = [pygame.image.load(os.path.join(os.curdir, 'groundenemy1.png')).convert_alpha(),
+                      pygame.image.load(os.path.join(os.curdir, 'groundenemy2.png')).convert_alpha(),
+                      pygame.image.load(os.path.join(os.curdir, 'groundenemy3.png')).convert_alpha(),
+                      pygame.image.load(os.path.join(os.curdir, 'groundenemy4.png')).convert_alpha(),
+                      pygame.image.load(os.path.join(os.curdir, 'groundenemy5.png')).convert_alpha(),
+                      pygame.image.load(os.path.join(os.curdir, 'groundenemy6.png')).convert_alpha(),
+                      pygame.image.load(os.path.join(os.curdir, 'groundenemy7.png')).convert_alpha(),
+                      pygame.image.load(os.path.join(os.curdir, 'groundenemy8.png')).convert_alpha()]
         self.rect = self.images[self.image].get_rect()
         if right == True:
             self.x = 1280
@@ -28,9 +30,18 @@ class Enemy(pygame.sprite.Sprite):
             self.speed = speed
         self.rect = self.rect.move(self.x, self.y)
 
+    def updateEnemySprite(self, framestart, totalframes):
+        self.framenumber += 0.33
+        if(self.framenumber > framestart + totalframes or self.framenumber < framestart):
+            self.framenumber = framestart
+        self.image = (int(self.framenumber)) % totalframes + framestart
 
     def updateEnemyPos(self,enemyList, index):
         self.image = (self.image + 1) % 8
+        if(self.right):
+            self.updateEnemySprite(4,4)
+        else:
+            self.updateEnemySprite(0,4)
         if self.x + self.speed < 513 or self.x + self.speed > 745:
             self.x += self.speed
             self.rect = self.rect.move(self.speed,0)
