b9db1e20859a8002e39f8bed75d13a8a5345c95a
Zcr1 <zcr1@pitt.edu>
2011-11-05 20:19:49 -0400

    Commit

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
 #Main
 import pygame, sys
 from pygame.locals import *
-
+from Player import *
 
 def main():
     pygame.init()
     fpsClock = pygame.time.Clock()
     windowSurfaceObj = pygame.display.set_mode((1280,720), DOUBLEBUF)
     pygame.display.set_caption("William Wallce Defender X-Treme 2140")
-    desertBackground = pygame.image.load('desert-background.jpg')
+    desertBackground = pygame.image.load('desert-background.jpg').convert()
+    player = Player()
     #Main Loop
     while True:
         windowSurfaceObj.blit(desertBackground,(0,0))
@@ -30,10 +31,22 @@ def main():
                     #scroll up or down
             elif event.type == KEYDOWN:
                 if event.key in (K_LEFT, K_RIGHT, K_UP, K_DOWN):
-                    blah = "blah"
+                    x = 0
+                    y = 0
+                    if K_LEFT:
+                        x = -1
+                    elif K_RIGHT:
+                        x = 1
+                    if K_UP:
+                        y = 1
+                    elif K_DOWN:
+                        y = -1
+                    player.updatePos(x)
+
                     #ARROW KEY
                 elif event.key == K_ESCAPE:
                     pygame.event.post(pygame.event.Event(QUIT))
+        windowSurfaceObj.blit(player.image,player.rect)
         pygame.display.update()
         #pygame.display.flip()
         fpsClock.tick(30)
