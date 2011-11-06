07616f414413340dc26f2b93da67881462723efd
Zcr1 <zcr1@pitt.edu>
2011-11-06 02:08:56 -0500

    commiting

--- Player.py
+++ Player.py
@@ -24,12 +24,11 @@ class Player(pygame.sprite.Sprite):
                       pygame.image.load(os.path.join(os.curdir, 'spritel8.png')).convert_alpha()]
         self.rect = self.images[self.image].get_rect()
         self.rect = self.rect.move(300,600)
-        self.vel = 10
-        self.x = 323
-        self.y = 624
+        self.vel = 0
+        self.x = 300
+        self.y = 600
         self.dir = 1 # 1: Right -1: Left
         self.vector = Vector(self.x,self.y)
-
 
     def updateVector(self,x,y):
 
@@ -49,17 +48,39 @@ class Player(pygame.sprite.Sprite):
 
     def updatePlayerPos(self,x,y):
         self.image = (self.image + 1) % 8
-        print self.x + x
-        print self.y + y
-        if self.x + x < 1280 and self.x + x > 10:
+        x = int(x)
+        y = int(y)
+        if self.x + x < 1252 and self.x + x > -5:
             self.x += x
             self.rect = self.rect.move(x,0)
-        if self.y + y < 630 and self.y + y > 10:
+        if self.y + y < 600 and self.y + y > 0:
             self.y += y
             self.rect = self.rect.move(0,y)
+        if self.y + y > 600:
+            self.rect = self.rect.move(0,600-self.y)
+            self.y = 600
 
     def updateArrowPos(self):
         self.x += self.vector.x * self.vel
         self.y += self.vector.y * self.vel
         self.rect = self.rect.move(self.vector.x * self.vel, self.vector.y * self.vel)
+
+    def jet(self):
+        if self.vel <= 0:
+            self.vel = 3
+        else:
+            self.vel *= 1.1
+            if self.vel > 10:
+                self.vel = 10
+        self.updatePlayerPos(0,-self.vel)
+
+    def fall(self):
+        self.vel -= 1
+        self.updatePlayerPos(0,-self.vel)
+
+
+    def arrow(self):
+        print "hi"
+
+
 
